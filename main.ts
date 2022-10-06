06836f89d09cff6dffa38ec378a8af35970c245e
Seanghay Yath <seanghay.dev@gmail.com>
2022-10-06 14:38:57 +0700

    chore: init

---
+++ main.ts
@@ -0,0 +1,29 @@
+import { Database } from "https://deno.land/x/sqlite3@0.6.1/mod.ts";
+import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+
+function handler(_req: Request) {
+	const t = performance.now();
+	const db = new Database(new URL("./db/file.db", import.meta.url));
+	const statement = db.prepare("SELECT * FROM dict LIMIT 100");
+	const items = [];
+
+	for (const row of statement) {
+		items.push(row);
+	}
+
+	db.close();
+	return new Response(
+		JSON.stringify({
+			elapsedMs: performance.now() - t,
+      items,
+
+		}),
+		{
+			headers: {
+				"content-type": "application/json",
+			},
+		}
+	);
+}
+
+serve(handler);
